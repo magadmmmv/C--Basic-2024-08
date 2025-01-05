1c1b8205eb7d9d8e1d1216277cc75b9b95f3e390
Magad Mammaev <MMammaev@it-one.ru>
2025-01-05 13:33:41 +0300

    Выполнил домашнее задание 7 (Анонимные типы, кортежи и лямбда-выражения)

---
+++ ДЗ 7 (Анонимные типы, кортежи и лямбда-выражения)/DZ7_3/DZ7_3/Program.cs
@@ -0,0 +1,90 @@
+﻿/*
+Программа 3.
+
+Скопировать решение из программы 2, но переделать метод "получить планету" так, чтобы он на вход принимал еще один параметр, описывающий способ защиты от слишком частых вызовов - делегат PlanetValidator
+(можно вместо него использовать Func), который на вход принимает название планеты, а на выходе дает строку с ошибкой. Метод "получить планету" теперь не должен проверять сколько вызовов делалось ранее.
+Вместо этого он должен просто вызвать PlanetValidator и передать в него название планеты, поиск которой производится. И если PlanetValidator вернул ошибку - передать ее на выход из метода третьим полем.
+
+Из main-метода при вызове "получить планету" в качестве нового параметра передавать лямбду, которая делает всё ту же проверку, которая была и ранее - на каждый третий вызов она возвращает строку
+"Вы спрашиваете слишком часто" (в остальных случаях возвращает null). Результат исполнения программы должен получиться идентичный программе 2.
+
+(*) Дописать main-метод так, чтобы еще раз проверять планеты "Земля", "Лимония" и "Марс", но передавать другую лямбду так, чтобы она для названия "Лимония" возвращала ошибку "Это запретная планета",
+ а для остальных названий - null. Убедиться, что в этой серии проверок ошибка появляется только для Лимонии.
+*/
+
+namespace DZ7_3
+{
+    internal class Program
+    {
+        class Planet
+        {
+            public string? Name { get; set; }
+            public int Number { get; set; }
+            public int Equator { get; set; }
+            public Planet? PreviousPlanet { get; set; }
+        }
+        public class PlanetCatalog
+        {
+
+            private Planet[] _catalogPlanet;
+            public int Count { get; set; }
+            public PlanetCatalog()
+            {
+                Count = 0;
+                Planet venera = new Planet
+                {
+                    Name = "Венера",
+                    Number = 2,
+                    Equator = 38_025,
+                    PreviousPlanet = null
+                };
+                Planet earth = new Planet
+                {
+                    Name = "Земля",
+                    Number = 3,
+                    Equator = 40_075,
+                    PreviousPlanet = venera
+                };
+                var mars = new Planet
+                {
+                    Name = "Марс",
+                    Number = 4,
+                    Equator = 21_344,
+                    PreviousPlanet = earth
+                };
+                _catalogPlanet = [venera, earth, mars];
+            }
+
+            public delegate string PlanetValidator(string name);
+
+            public (int? number, int? equator, string? exeption) GetPlanet(string name, PlanetValidator planetValidator)
+            {
+                Count++;
+
+                foreach (var planet in _catalogPlanet)
+                    if (planet.Name == name)
+                        return (number: planet.Number, equator: planet.Equator, exeption: planetValidator(name));
+
+                return (number: null, equator: null, exeption: planetValidator(name) != null ? planetValidator(name) : "Не удалось найти планету");
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Программа 3.
+            Console.WriteLine("Программа 3");
+            var planets = new PlanetCatalog();
+
+            Console.WriteLine("Земля: " + planets.GetPlanet("Земля", y => planets.Count % 3 == 0 ? "Вы спрашиваете слишком часто" : null!));
+            Console.WriteLine("Лимония: " + planets.GetPlanet("Лимония", y => planets.Count % 3 == 0 ? "Вы спрашиваете слишком часто" : null!));
+            Console.WriteLine("Марс: " + planets.GetPlanet("Марс", y => planets.Count % 3 == 0 ? "Вы спрашиваете слишком часто" : null!));
+
+            Console.WriteLine();
+
+            // (*)
+            Console.WriteLine("Доп. задание (*)");
+            Console.WriteLine("Земля: " + planets.GetPlanet("Земля", y => y == "Лимония" ? "Это запретная планета" : null!));
+            Console.WriteLine("Лимония: " + planets.GetPlanet("Лимония", y => y == "Лимония" ? "Это запретная планета" : null!));
+            Console.WriteLine("Марс: " + planets.GetPlanet("Марс", y => y == "Лимония" ? "Это запретная планета" : null!));
+        }
+    }
+}
