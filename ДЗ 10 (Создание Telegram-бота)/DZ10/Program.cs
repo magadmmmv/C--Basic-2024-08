0199b7507e32d4a6bc756968726f55ce93b2bfac
Magad Mammaev <MMammaev@it-one.ru>
2025-03-09 19:52:48 +0300

    Выполнил ДЗ 10 (Создание Telegram бота)

---
+++ ДЗ 10 (Создание Telegram-бота)/DZ10/Program.cs
@@ -0,0 +1,69 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+
+namespace DZ10
+{
+    internal class Program
+    {
+        // replace YOUR_BOT_TOKEN below, or set your BOT_TOKEN in Project Properties > Debug > Launch profiles UI > Environment variables
+        private static string _botKey = Environment.GetEnvironmentVariable("BOT_TOKEN") ?? "YOUR_BOT_TOKEN";
+
+        private static async Task Main()
+        {
+            using var cts = new CancellationTokenSource();
+            var botClient = new TelegramBotClient(_botKey, cancellationToken: cts.Token);
+
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = [UpdateType.Message],
+                DropPendingUpdates = true
+            };
+            var handler = new UpdateHandler();            
+
+            try
+            {
+                Console.WriteLine("Нажмите клавишу A для выхода");
+
+                if (Console.ReadKey(true).Key == ConsoleKey.A)
+                    cts.Cancel(); // stop the bot
+                else
+                {
+                    var me = await botClient.GetMe(cts.Token);
+                    Console.WriteLine($"{me.FirstName} запущен!");
+                }                    
+
+                botClient.StartReceiving(handler, receiverOptions);
+
+                handler.OnHandleUpdateStarted += PrintConsoleMessageStarted;
+                handler.OnHandleUpdateCompleted += PrintConsoleMessageCompleted;                
+
+                await Task.Delay(-1, cts.Token); // We set an infinite delay so that the bot runs continuously with CancellationTokenSource
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Async operation was canceled.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            finally
+            {
+                handler.OnHandleUpdateStarted -= PrintConsoleMessageStarted;
+                handler.OnHandleUpdateCompleted -= PrintConsoleMessageCompleted;
+            }
+        }
+
+        public static void PrintConsoleMessageStarted(Message message)
+        {
+            Console.WriteLine($"Началась обработка сообщения '{message.Text ?? message.Type.ToString()}'");
+        }
+        public static void PrintConsoleMessageCompleted(Message message)
+        {
+            Console.WriteLine($"Закончилась обработка сообщения '{message.Text ?? message.Type.ToString()}'");
+        }
+    }    
+}
---
+++ ДЗ 10 (Создание Telegram-бота)/DZ10/UpdateHandler.cs
@@ -0,0 +1,67 @@
+﻿using System.Net.Http.Json;
+using Telegram.Bot.Polling;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace DZ10
+{
+    public class UpdateHandler : IUpdateHandler
+    {
+        public delegate void MessageHandler(Message message);
+        public event MessageHandler? OnHandleUpdateStarted;
+        public event MessageHandler? OnHandleUpdateCompleted;
+
+        record CatFactDto(string Fact, int length);
+        public async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, HandleErrorSource source, CancellationToken cancellationToken)
+        {
+            Console.WriteLine(exception);
+            await Task.FromException(exception);
+        }
+
+        public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            try
+            {
+                switch (update.Type)
+                {
+                    case UpdateType.Message:
+                        await BotOnMessageReceived(botClient, update.Message!, cancellationToken);
+                        break;
+                }
+            }
+            catch (Exception exception)
+            {
+                await HandleErrorAsync(botClient, exception, HandleErrorSource.PollingError, cancellationToken);
+            }
+        }
+
+        public async Task BotOnMessageReceived(ITelegramBotClient botClient, Message message, CancellationToken cancellationToken)
+        {
+            if (message.Type != MessageType.Text)
+                return;
+
+            OnHandleUpdateStarted?.Invoke(message);
+
+            var action = message.Text!.Split(' ')[0];
+            switch (action)
+            {
+                case "/cat":
+                    await SendMessageCatFact(botClient, message, cancellationToken);
+                    break;
+                default:
+                    await botClient.SendMessage(chatId: message.Chat.Id, "Сообщение успешно принято", cancellationToken: cancellationToken);
+                    break;
+            }
+
+            OnHandleUpdateCompleted?.Invoke(message);
+        }
+
+        public async Task SendMessageCatFact(ITelegramBotClient botClient, Message message, CancellationToken token)
+        {
+            using var client = new HttpClient();
+            var catFact = await client.GetFromJsonAsync<CatFactDto>("https://catfact.ninja/fact", token);
+            await botClient.SendMessage(chatId: message.Chat.Id, text: catFact!.Fact, cancellationToken: token);
+        }
+    }
+}
